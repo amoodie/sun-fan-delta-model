040b2adbef4cc7439750427ab118f4e915259755
Ajay Limaye <tiburon4@gmail.com>
2021-08-12 09:03:56 -0400

    move topo update to function

--- sunFanModel.m
+++ sunFanModel.m
@@ -147,76 +147,11 @@ end
             end
         end
         
-        % update bed elevation along flow paths defined by channel segments (eqn. 12)
-        grid.Qs_in = zeros(grid.size);
-        for i=1:grid.size(1)
-            for j=1:grid.size(2)
-                % look up the sediment discharge into the grid cell
-                % If this is the first segment, then use inlet
-                % sediment discharge. Otherwise, check the sediment
-                % discharge to this cell using grid.flowsFrom
-                if i==inlet.row && j==inlet.col
-                    grid.Qs_in(i,j) = Qs_inlet;
-                else            
-                    % look up the contributing cells
-                    contributingCells = grid.flowsFrom{i,j};
-                    
-                    if numel(contributingCells)==1
-                        grid.Qs_in(i,j) = grid.Qs_out(contributingCells);
-                    elseif numel(contributingCells)>1 % multiple contributing cells
-                        for k=1:numel(contributingCells)
-                            % Sediment routing at network junctions is not explictly
-                            % discussed in the paper. For simplicitly, I assume
-                            % that the sediment discharge is partitioned in the
-                            % same way that water discharge is partitioned.
-
-                            % look up Qs_out at the contributing cell; use the water discharge partition fraction at
-                            % at the tributary to set the partition fraction
-                            % for sediment
-                            Qs_out_fromContributingCell = grid.Qs_out(contributingCells(k));
-                            % Qs_out_fromContributingCell is sometimes empty:
-                            % if discharge did not reach this location due to routeFlow rules,
-                            % depsite these cells being connected by a channel in the tracking cell arrays.
-                            % We thus include a safety here, to just proceed with the Qs calculation,
-                            % but with 0 sediment contribution from this cell
-                            if Qs_out_fromContributingCell > 0
-                                Qw_fraction_receivedFromContributingCell = grid.flowsToFrac_Qw_distributed{contributingCells(k)};  
-                                [receivingCellRow,receivingCellCol] = ind2sub(grid.size,grid.flowsTo{contributingCells(k)});
-                                if i==receivingCellRow(1) && j==receivingCellCol(1)
-                                    Qw_fraction_receivedFromContributingCell = Qw_fraction_receivedFromContributingCell(1);
-                                elseif i==receivingCellRow(2) && j==receivingCellCol(2)
-                                    Qw_fraction_receivedFromContributingCell = Qw_fraction_receivedFromContributingCell(2);
-                                else
-                                    error('Error in data lookup for contributing cell')
-                                end
-                                grid.Qs_in(i,j) = grid.Qs_in(i,j) + Qs_out_fromContributingCell*Qw_fraction_receivedFromContributingCell;
-                            end
-                        end
-                    else
-                        % throw error if this cell is flagged as a channel
-                        % but there are no contributingCells defined,
-                        % unless this is the inlet cell (which has no
-                        % contributing cells)
-                        if grid.channelFlag(i,j) && ne(i,inlet.row) && ne(j,inlet.col)
-                            error('No contributing cells found where they were expected');
-                        end
-                    end
-                end
-            end
-        end
+        % update bed elevation along flow paths
+        grid = updateTopography(grid,inlet,lambda,tStep_sec); 
         
-        % set Qs_in, Qs_out to zero for non-channel cells (values are
-        % currently NaN)
-        grid.Qs_in(~grid.channelFlag)=0;
-        grid.Qs_out(~grid.channelFlag)=0;
-        
-        % conservation of mass (equation 12)
-        grid.deltaz = ((grid.Qs_in-grid.Qs_out)/((1-lambda)*grid.dx^2))*tStep_sec; % equivalent to  -((Qs_out-Qs_in)/((1-lambda)*grid.dx^2))*tStep; 
-        
-        % update topography for all grid cells
-        grid.z = grid.z + grid.deltaz;
-        
-        grid = updateSlope(grid,boundaryCondition,t); % update slope arrays following topography update
+        % update slope arrays following topography update
+        grid = updateSlope(grid,boundaryCondition,t); 
 
         % update grid.oceanFlag following topography update
         grid.oceanFlag = grid.z <= oceanLevel;
