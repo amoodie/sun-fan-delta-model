6d1c859040005c717e83bef7d9524b1a2f4024d2
Andrew Moodie <amoodie@utexas.edu>
2021-08-19 17:37:19 -0500

    basic documentation

--- source/plot/gridLabelChannelInds.m
+++ source/plot/gridLabelChannelInds.m
@@ -1,5 +1,8 @@
 function gridLabelChannelInds(grid)
-
+%gridLabelChannelInds Add cell index labels to a plot
+% Adds the cell indices of cells marked by `grid.channelFlag` to the
+% current axis.
+    
     for k=1:numel(grid.x)
         if grid.channelFlag(k)
             [x,y] = ind2sub(grid.size, k);
