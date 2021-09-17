fec59b015e41d9d85dc9ef783815adb0485aadd8
Andrew Moodie <amoodie@utexas.edu>
2021-09-17 14:02:41 -0500

    change to only looping through the channel locations

--- source/updateTopography.m
+++ source/updateTopography.m
@@ -1,57 +1,65 @@
 function grid = updateTopography(grid,inlet,lambda,tStep_sec,Qs_inlet)    
 % updateTopography.m: updates bed elevation along flow paths using Equation 12.
     grid.Qs_in = zeros(grid.size);
-    for i=1:grid.size(1)
-        for j=1:grid.size(2)
-            % look up the sediment discharge into the grid cell
-            % If this is the first segment, then use inlet
-            % sediment discharge. Otherwise, check the sediment
-            % discharge to this cell using grid.flowsFrom
-            if i==inlet.row && j==inlet.col
-                grid.Qs_in(i,j) = Qs_inlet;
-            else            
-                % look up the contributing cells
-                contributingCells = grid.flowsFrom{i,j};
 
-                if numel(contributingCells)==1
-                    grid.Qs_in(i,j) = grid.Qs_out(contributingCells);
-                elseif numel(contributingCells)>1 % multiple contributing cells
-                    for k=1:numel(contributingCells)
-                        % Sediment routing at network junctions is not explictly
-                        % discussed in the paper. For simplicitly, I assume
-                        % that the sediment discharge is partitioned in the
-                        % same way that water discharge is partitioned.
+    % determine all the channel locations to loop through
+    channelInds = find(grid.channelFlag);
 
-                        % look up Qs_out at the contributing cell; use the water discharge partition fraction at
-                        % at the tributary to set the partition fraction
-                        % for sediment
-                        Qs_out_fromContributingCell = grid.Qs_out(contributingCells(k));
-                        % Qs_out_fromContributingCell is sometimes empty:
-                        % if discharge did not reach this location due to routeFlow rules,
-                        % depsite these cells being connected by a channel in the tracking cell arrays.
-                        % We thus include a safety here, to just proceed with the Qs calculation,
-                        % but with 0 sediment contribution from this cell
-                        if Qs_out_fromContributingCell > 0
-                            Qw_fraction_receivedFromContributingCell = grid.flowsToFrac_Qw_distributed{contributingCells(k)};  
-                            [receivingCellRow,receivingCellCol] = ind2sub(grid.size,grid.flowsTo{contributingCells(k)});
-                            if i==receivingCellRow(1) && j==receivingCellCol(1)
-                                Qw_fraction_receivedFromContributingCell = Qw_fraction_receivedFromContributingCell(1);
-                            elseif i==receivingCellRow(2) && j==receivingCellCol(2)
-                                Qw_fraction_receivedFromContributingCell = Qw_fraction_receivedFromContributingCell(2);
-                            else
-                                error('Error in data lookup for contributing cell')
-                            end
-                            grid.Qs_in(i,j) = grid.Qs_in(i,j) + Qs_out_fromContributingCell*Qw_fraction_receivedFromContributingCell;
+    % loop through the channel inds
+    for kk=1:numel(channelInds)
+
+        % grab this channelInd and conver to row,col
+        k = channelInds(kk);
+        [i,j] = ind2sub(grid.size,k);
+
+        % look up the sediment discharge into the grid cell
+        % If this is the first segment, then use inlet
+        % sediment discharge. Otherwise, check the sediment
+        % discharge to this cell using grid.flowsFrom
+        if i==inlet.row && j==inlet.col
+            grid.Qs_in(i,j) = Qs_inlet;
+        else
+            % look up the contributing cells
+            contributingCells = grid.flowsFrom{i,j};
+
+            if numel(contributingCells)==1
+                grid.Qs_in(i,j) = grid.Qs_out(contributingCells);
+            elseif numel(contributingCells)>1 % multiple contributing cells
+                for k=1:numel(contributingCells)
+                    % Sediment routing at network junctions is not explictly
+                    % discussed in the paper. For simplicitly, I assume
+                    % that the sediment discharge is partitioned in the
+                    % same way that water discharge is partitioned.
+
+                    % look up Qs_out at the contributing cell; use the water discharge partition fraction at
+                    % at the tributary to set the partition fraction
+                    % for sediment
+                    Qs_out_fromContributingCell = grid.Qs_out(contributingCells(k));
+                    % Qs_out_fromContributingCell is sometimes empty:
+                    % if discharge did not reach this location due to routeFlow rules,
+                    % depsite these cells being connected by a channel in the tracking cell arrays.
+                    % We thus include a safety here, to just proceed with the Qs calculation,
+                    % but with 0 sediment contribution from this cell
+                    if Qs_out_fromContributingCell > 0
+                        Qw_fraction_receivedFromContributingCell = grid.flowsToFrac_Qw_distributed{contributingCells(k)};  
+                        [receivingCellRow,receivingCellCol] = ind2sub(grid.size,grid.flowsTo{contributingCells(k)});
+                        if i==receivingCellRow(1) && j==receivingCellCol(1)
+                            Qw_fraction_receivedFromContributingCell = Qw_fraction_receivedFromContributingCell(1);
+                        elseif i==receivingCellRow(2) && j==receivingCellCol(2)
+                            Qw_fraction_receivedFromContributingCell = Qw_fraction_receivedFromContributingCell(2);
+                        else
+                            error('Error in data lookup for contributing cell')
                         end
+                        grid.Qs_in(i,j) = grid.Qs_in(i,j) + Qs_out_fromContributingCell*Qw_fraction_receivedFromContributingCell;
                     end
-                else
-                    % throw error if this cell is flagged as a channel
-                    % but there are no contributingCells defined,
-                    % unless this is the inlet cell (which has no
-                    % contributing cells)
-                    if grid.channelFlag(i,j) && ne(i,inlet.row) && ne(j,inlet.col)
-                        error('No contributing cells found where they were expected');
-                    end
+                end
+            else
+                % throw error if this cell is flagged as a channel
+                % but there are no contributingCells defined,
+                % unless this is the inlet cell (which has no
+                % contributing cells)
+                if grid.channelFlag(i,j) && ne(i,inlet.row) && ne(j,inlet.col)
+                    error('No contributing cells found where they were expected');
                 end
             end
         end
