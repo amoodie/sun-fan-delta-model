858bc64fb4ee97aa1fc47a0610eb8e49c6a6044f
Andrew Moodie <amoodie@utexas.edu>
2021-10-21 15:23:40 -0500

    implement probabilistic routing and refactor flow routing

--- source/routeFlow.m
+++ source/routeFlow.m
@@ -1,9 +1,10 @@
 function grid=routeFlow(grid,inlet,Qw_inlet,gamma,Qw_mismatch_tolerance)
-     
+        %% set up the routing
         % routeFlow: routes flow along the channel network, splitting flow at branches using eqn. 11.
         grid.Qw = zeros(grid.size); % initialize grid that stores total water discharge during flow routing
-        grid.Qw_toRoute = zeros(grid.size); % initialize grid that stores water discharge to route 
+        grid.Qw_toRoute = nan(grid.size); % initialize grid that stores water discharge to route 
         grid.flowsToFrac_Qw_distributed = cell(grid.size); % initialize grid to track fractional distribution of flow at distributaries
+        alreadyFlowed = zeros(grid.size); % initialize grid to track where flow already visited (looping check)
         
         % To begin the routing, the only cell with flow is the inlet
         % cell; specify that discharge in Qw_grid
@@ -19,7 +20,19 @@ function grid=routeFlow(grid,inlet,Qw_inlet,gamma,Qw_mismatch_tolerance)
         % identify network endpoints as cells that do not flow into any
         % other cells
         grid.networkEndpoints = (grid.flowsToCount == 0);
-
+        
+        % determine the count of flows from
+        grid.flowsFromCount = zeros(grid.size);
+        for k=1:numel(grid.channelFlag)
+            grid.flowsFromCount(k) = numel(grid.flowsFrom{k});
+        end
+        
+        % set the tracker for places that have all of their inputs filled
+        grid.flowedFromSources = zeros(grid.size);
+        grid.flowedFromSources(inlet.row,inlet.col) = 1;
+        
+        %% run the routing
+        % while there are any cells that have flow to route
         while ~isempty(cellIndFlowToRoute) % route flow while there are still cells flagged to route from
             % for each cell with flow to route, route its flow to next cell
             % along the flow path, pausing at any bifurcations (i.e.,
@@ -42,15 +55,21 @@ function grid=routeFlow(grid,inlet,Qw_inlet,gamma,Qw_mismatch_tolerance)
             cellsIndFlowsTo = grid.flowsTo{cellIndFlowToRoute};
             
             if isempty(cellsIndFlowsTo)
-                grid.Qw_toRoute(cellIndFlowToRoute)=0;
+                % doesn't flow anywhere
+                %   set the current cell to NaN and leave
+                grid.Qw_toRoute(cellIndFlowToRoute)=nan;
             elseif numel(cellsIndFlowsTo)==1
-                grid.Qw_toRoute(cellsIndFlowsTo) = Qw_routed;
+                % flows to one location
+                %   set the next cell to have the current cell's total flow (plus whatever it already had!)
+                grid.Qw_toRoute(cellsIndFlowsTo) = sum([grid.Qw_toRoute(cellsIndFlowsTo), Qw_routed],'omitnan');
                 grid.Qw(cellsIndFlowsTo) =  grid.Qw(cellsIndFlowsTo) + Qw_routed;
+                % update partitioning information for current cell
                 grid.flowsToFrac_Qw_distributed{cellIndFlowToRoute} = 1; % i.e., all flow distributed to 1 cell
-                grid.Qw_toRoute(cellIndFlowToRoute) = 0;
+                % update flow information for current cell to empty
+                grid.Qw_toRoute(cellIndFlowToRoute) = nan;
+                grid.flowedFromSources(cellsIndFlowsTo) = grid.flowedFromSources(cellsIndFlowsTo) + 1;
             elseif numel(cellsIndFlowsTo)==2
-                
-                % Flow has reached a distributary junction.
+                % flow has reached a distributary junction
                 [source.row,source.col] = ind2sub(grid.size,cellIndFlowToRoute);
                 [branch1.startRow,branch1.startCol] = ind2sub(grid.size,cellsIndFlowsTo(1));
                 [branch2.startRow,branch2.startCol] = ind2sub(grid.size,cellsIndFlowsTo(2));
@@ -79,11 +98,11 @@ function grid=routeFlow(grid,inlet,Qw_inlet,gamma,Qw_mismatch_tolerance)
                         branch2.Qw_fractionReceived = 1-branch1.Qw_fractionReceived;
                     end
                 elseif branch1.S <= 0 && branch2.S > 0
-                   branch1.Qw_fractionReceived = 0;
+                    branch1.Qw_fractionReceived = 0;
                     branch2.Qw_fractionReceived = 1;
                 elseif branch1.S > 0 && branch2.S <= 0
-                    branch1.Qw_fractionReceived = 1;
                     branch2.Qw_fractionReceived = 0;
+                    branch1.Qw_fractionReceived = 1;
                 else
                     % Distribute flow between downstream cells using equation 11. 
                     branch1.Qw_fractionReceived= (branch1.S^gamma)/((branch1.S^gamma + branch2.S^gamma)); % fraction of flow apportioned to the new branch (equation 11)
@@ -99,7 +118,7 @@ function grid=routeFlow(grid,inlet,Qw_inlet,gamma,Qw_mismatch_tolerance)
                 % cell
                 if branch1.Qw_received<0 || branch2.Qw_received<0
                     error('Negative discharge routed at distributary junction')
-                elseif abs(grid.Qw_toRoute(cellIndFlowToRoute) - (branch1.Qw_received + branch2.Qw_received)) > (100*eps)
+                elseif abs(grid.Qw_toRoute(cellIndFlowToRoute) - (branch1.Qw_received + branch2.Qw_received)) > (Qw_mismatch_tolerance)
                     error('Inconsistent mass balance for discharge at distributary junction')
                 end
                 
@@ -107,9 +126,9 @@ function grid=routeFlow(grid,inlet,Qw_inlet,gamma,Qw_mismatch_tolerance)
                 grid.Qw(branch1.startRow,branch1.startCol) =  grid.Qw(branch1.startRow,branch1.startCol) + branch1.Qw_received;
                 grid.Qw(branch2.startRow,branch2.startCol) =  grid.Qw(branch2.startRow,branch2.startCol) + branch2.Qw_received;
 
-                grid.Qw_toRoute(branch1.startRow,branch1.startCol) = grid.Qw_toRoute(branch1.startRow,branch1.startCol) + branch1.Qw_received;
-                grid.Qw_toRoute(branch2.startRow,branch2.startCol) = grid.Qw_toRoute(branch2.startRow,branch2.startCol) + branch2.Qw_received;
-                grid.Qw_toRoute(source.row,source.col) = 0;
+                grid.Qw_toRoute(branch1.startRow,branch1.startCol) = sum([grid.Qw_toRoute(branch1.startRow,branch1.startCol), branch1.Qw_received],'omitnan');
+                grid.Qw_toRoute(branch2.startRow,branch2.startCol) = sum([grid.Qw_toRoute(branch2.startRow,branch2.startCol), branch2.Qw_received],'omitnan');
+                grid.Qw_toRoute(source.row,source.col) = nan;
 
                 % Record the flow partitioning information in the
                 % corresponding grid and grid cell
@@ -117,35 +136,48 @@ function grid=routeFlow(grid,inlet,Qw_inlet,gamma,Qw_mismatch_tolerance)
                 if numel(grid.flowsToFrac_Qw_distributed{source.row,source.col}) ~= 2
                     error('Error in defining discharge routing to multiple branches');
                 end
+                
+                % record the flow having gone into each of the cells
+                grid.flowedFromSources(branch1.startRow,branch1.startCol) = grid.flowedFromSources(branch1.startRow,branch1.startCol) + 1;
+                grid.flowedFromSources(branch2.startRow,branch2.startCol) = grid.flowedFromSources(branch2.startRow,branch2.startCol) + 1;
             else
                 error('Flow routing: Invalid value for cellsIndFlowsTo');
             end
-
-            % update list of cells from which to route flow for next
-            % iteration
-            cellIndFlowToRoute = find(grid.Qw_toRoute>eps,1,'first');
+            
+            % update the status of the loop checking array for the current cell
+            alreadyFlowed(cellIndFlowToRoute) = 1;
+            
+            % update list of cells from which to route flow for next iteration
+            %   find where there is water to route
+            hasFlow = ~isnan(grid.Qw_toRoute);
+            % find where cells have received flow from all upstream cells
+            hasSources = (grid.flowedFromSources == grid.flowsFromCount);
+            % find where a cell is ready to flow
+            readyToFlow = and(hasFlow,hasSources);
+            % places that haven't already been flowed (prevents loops)
+            notAlreadyFlowed = ~(alreadyFlowed);  % prevents loops
+            
+            % REMOVE THIS CHECK (TRYING TO FIND A LOOP!!)
+            if any(and(alreadyFlowed,hasFlow))
+                breakpoint()
+            end
+            
+            % choose the next cell to flow as any one that is ready to
+            %    flow, but hasn't already flowed
+            cellIndFlowToRoute = find(and(readyToFlow,notAlreadyFlowed),1,'first');
+            
         end
         
-        % flow routing is now complete. Run some diagnostic checks:
+        %% flow routing is now complete. Run some diagnostic checks:
         % (1) Check that no cell has a discharge greater than the inlet discharges 
         if any((grid.Qw(:) - Qw_inlet) > Qw_mismatch_tolerance)
             error('Error in flow routing: discharge greater than input discharge detected')
         end
-        
-        %%%% this threw an error for reasons I don't fully understand. In
-        %%%% principle, I think you should be able to compare inlet to sum
-        %%%% of endpoints and get same discharge. I got mismatches using
-        %%%% this code, but then when I check step-by-step for each routing
-        %%%% operation I can't see any mismatch between input and output.
-        % sum the discharge for the network endpoints and verify that
-        % it equals the input discharge
-        
-% %         Qw_out_total = sum(grid.Qw(grid.networkEndpoints));
-% % 
-% %         % Compare the summed discharge at outlets to the discharge at
-% %         % the inlet and through an error if their difference exceeds
-% %         % numerical precision
-% %         if abs(Qw_out_total - Qw_inlet) > 100*eps
-% %             error('flowRoute: Summed discharge at channel network outlets does not equal discharge at inlet');
-% %         end
+       
+        % (2) sum the discharge for the network endpoints and verify that
+        %    it equals the input discharge
+        Qw_out_total = sum(grid.Qw(grid.networkEndpoints));
+        if abs(Qw_out_total - Qw_inlet) > Qw_mismatch_tolerance
+            error('flowRoute: Summed discharge at channel network outlets does not equal discharge at inlet');
+        end
     end % end nested function routeFlow
